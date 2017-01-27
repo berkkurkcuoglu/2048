6a47ab66cf8bc748fabb51674f28fb93559c77ff
berk <berk@berks-imac.local>
2017-01-27 00:04:59 -0500

    Initial Commit

---
+++ 2048/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  2048
+//
+//  Created by berk on 1/27/17.
+//  Copyright © 2017 berk. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ 2048/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  2048
+//
+//  Created by berk on 1/27/17.
+//  Copyright © 2017 berk. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
